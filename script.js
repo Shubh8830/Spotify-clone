900ce3f65ef85fcc960a4e3c407a2b8811aeec34
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-05-29 08:59:42 +0530

    .

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 
 //Initialize the variables
 let songIndex = 0;
-let audioElement = new Audio('1.mp3');
+let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar =document.getElementById('myProgressBar');
 
 let songs = [
-    {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "Salam-e-Ishq", filePath: " songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: " songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: " songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: " songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: " songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: " songs/1.mp3", coverPath: "covers/1.jpg"}
 ]
 
 //audioElement.play();
@@ -27,4 +27,5 @@ masterPlay.addEventListener('click', ()=>{
 //Listen to Events
 myProgressBar.addEventListener('timeupdate', ()=>{
     console.log('timeupdate');
+    //update seek bar
 })
