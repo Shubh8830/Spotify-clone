75aa221e267bd9cea19ee76be75b7cb9ac80569a
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-05-29 08:42:43 +0530

    play feture

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 console.log('welcome to javascript');
+
+//Initialize the variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+
 let songs = [
     {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
     {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
@@ -7,4 +13,8 @@ let songs = [
     {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
     {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"}
 ]
-let audioElement = new Audio()
+
+//audioElement.play();
+
+//Listen to Events
+document.addEventListener('time')
