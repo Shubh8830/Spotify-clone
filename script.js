d9a825290313130132157facc53d822ee6cf9923
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-05-29 08:37:43 +0530

    html css is done now

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('welcome to javascript');
+let songs = [
+    {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
+]
+let audioElement = new Audio()
