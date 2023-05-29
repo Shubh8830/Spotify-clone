a7d8a9e039fd00b8589e2aaa3f309f93013ced5f
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-05-29 08:38:45 +0530

    js going start

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ let songs = [
     {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
     {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
     {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: " song/1.mp3", coverPath: "covers/1.jpg"}
 ]
 let audioElement = new Audio()
