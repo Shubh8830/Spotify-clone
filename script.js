fdb6a45be1cff710f89ee0be9c1a30db56e8f221
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-05-29 09:07:36 +0530

    first song play done

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar =document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
 
 let songs = [
     {songName: "Salam-e-Ishq", filePath: " songs/1.mp3", coverPath: "covers/1.jpg"},
@@ -20,6 +21,15 @@ let songs = [
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity =1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity =0;
     }
 
 })
@@ -28,4 +38,5 @@ masterPlay.addEventListener('click', ()=>{
 myProgressBar.addEventListener('timeupdate', ()=>{
     console.log('timeupdate');
     //update seek bar
+
 })
